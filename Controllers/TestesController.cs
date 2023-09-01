e787ef62306c88ddad5903b1b8bb4848c58c3998
Guilherme Cardoso <guilhermecardosor2004@gmail.com>
2023-08-31 21:22:45 -0300

    modulo3-aula06 finalizada

--- Controllers/TestesController.cs
+++ Controllers/TestesController.cs
@@ -1,8 +1,12 @@
+using System.Text.Json.Serialization;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
 namespace PrimeiraApp.Controllers
 {
+    [Route("/", Order = 0)]
+    [Route("minha-conta", Order = 1)]
+    [Route("gestao-da-conta", Order = 2)]
     public class TestesController : Controller
     {
         public ActionResult Index()
@@ -10,17 +14,19 @@ namespace PrimeiraApp.Controllers
             return View();
         }
 
+        [HttpGet("detalhes/{id:int}")]
         public ActionResult Details(int id)
         {
             return View();
         }
 
+        [HttpGet("novo")]
         public ActionResult Create()
         {
             return View();
         }
 
-        [HttpPost]
+        [HttpPost("novo")]
         [ValidateAntiForgeryToken]
         public ActionResult Create(IFormCollection collection)
         {
@@ -34,12 +40,13 @@ namespace PrimeiraApp.Controllers
             }
         }
 
+        [HttpGet("editar/{id:int}")]
         public ActionResult Edit(int id)
         {
             return View();
         }
 
-        [HttpPost]
+        [HttpPost("editar/{id:int}")]
         [ValidateAntiForgeryToken]
         public ActionResult Edit(int? id, IFormCollection collection)
         {
@@ -52,12 +59,14 @@ namespace PrimeiraApp.Controllers
                 return View();
             }
         }
+
+        [HttpGet("deletar/{id:int}")]
         public ActionResult Delete(int id)
         {
             return View();
         }
 
-        [HttpPost]
+        [HttpPost("deletar/{id:int}")]
         [ValidateAntiForgeryToken]
         public ActionResult Delete(int id, IFormCollection collection)
         {
